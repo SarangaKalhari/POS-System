95a62108b61781af8f97815b30e3575ebf570222
SarangaKalhari <kalharisaranga9@gmail.com>
2025-08-27 03:20:19 +0530

    calculate  price selected items

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,26 +24,53 @@ function getItem(button) {
 window.onload = function () {
     if (window.location.pathname.includes("cart.html")) {
         const cart = JSON.parse(localStorage.getItem("cart")) || [];
+        
+        // const isSelected=document.getElementById("cart-select") ;
         const list = document.getElementById("cart-items");
         const totalContainer = document.getElementById("total-price");
 
-        let total = 0;
+        // let total = 0;
 
-        if (cart.length === 0) {
+        if (cart.length === 0 ) {
             list.innerHTML = "<li>Your cart is empty.</li>";
             totalContainer.textContent = "Total: $0.00";
         } else {
             cart.forEach(item => {
                 const li = document.createElement("li");
-                li.textContent = `${item.title} - ${item.price}`;
+
+                const checkbox=document.createElement("input");
+                checkbox.type="checkbox";
+                checkbox.className = "cart-check";
+                checkbox.dataset.price = item.price;
+
+                checkbox.addEventListener("change",calculateSelectedTotal);
+
+                li.appendChild(checkbox);
+
+                const text = document.createTextNode(`${item.title} - ${item.price}`);
+                li.appendChild(text);
+
+                
                 list.appendChild(li);
 
                 // Extract number from price string like "$70.00"
-                let numericPrice = parseFloat(item.price.replace(/[^0-9.]/g, ""));
-                total += numericPrice;
+                
             });
 
-            totalContainer.textContent = `Total: $${total.toFixed(2)}`;
+            // totalContainer.textContent = `Total: $${total.toFixed(2)}`;
         }
     }
 };
+
+function calculateSelectedTotal() {
+    const checkboxes= document.querySelectorAll(".cart-check");
+    let total = 0;
+
+    checkboxes.forEach(cb => {
+        if(cb.checked){
+            let numericPrice = parseFloat(cb.dataset.price.replace(/[^0-9.]/g, ""));
+            total += numericPrice;
+        }
+    });
+    document.getElementById("total-price").textContent = `Total: $${total.toFixed(2)}`;
+}
